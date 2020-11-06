c9b8020e0d91b6a557cc218238e4317795f49170
rasul_gasbanov <gasbanovrasul@gmail.com>
2020-11-06 17:33:27 +0300

    project-test

---
+++ js/scripts.js
@@ -0,0 +1,88 @@
+"use strict"; //строгий режим
+
+
+
+
+//создаем экземпляр vue
+
+var app = new Vue({              
+  el: "#app",
+  data: function data() {
+    return {
+      isActive: true,
+      products: [],
+    };
+  },
+  mounted: function mounted() {
+    var self = this;
+    $.ajax({
+      url: "js/products.json",
+      method: "GET",
+      success: function success(data) {
+        self.products = data;
+      },
+      error: function error(_error) {
+        console.log(_error);
+      },
+    });
+  },
+  //2.	Для картинок использовать строковый модификатор  ‘_220x220_1’. (т.е. этот модификатор должен появиться перед расширением файла)
+  filters: {
+    removeZeros: function removeZeros(value) {
+      return value.replace(/^0+/, "");
+    },
+    addModifier: function addModifier(img) {
+      var imgName = img.split(".");
+      imgName[imgName.length - 2] += "_220x220_1";
+
+      return imgName.join(".");
+    },
+    roundValue: function roundValue(val) {
+      return Math.round(val * 100) / 100;
+    },
+    unitChange: function unitChange(val) {
+      if (val == "метр погонный") {
+        return "погон. метрами";
+      } else {
+        return val + "ми";
+      }
+    },
+  },
+  //Вывод цен 
+  methods: {
+    setRetailPrice: function setRetailPrice(val) {
+      if (val.isActive) {
+        return val.priceRetail;
+      } else {
+        return val.priceRetailAlt;
+      }
+    },
+    setGoldenPrice: function setGoldenPrice(val) {
+      if (val.isActive) {
+        return val.priceGold;
+      } else {
+        return val.priceGoldAlt;
+      }
+    },
+  },
+//3.	По клику изменяется количество товара. (изначально товар 1)
+  components: {
+    "counter-block": {
+      data: function data() {
+        return {
+          counter: 1,
+        };
+      },
+      template:
+        '<div class="stepper">                <input class="product__count stepper-input" type="text" :value="counter" >                <span class="stepper-arrow up" v-on:click="increase"></span>                <span class="stepper-arrow down" v-on:click="decrease"></span>              </div>\n              ',
+      methods: {
+        increase: function increase() {
+          this.counter++;
+        },
+        decrease: function decrease() {
+          if (this.counter > 0) this.counter--;
+        },
+      },
+    },
+  },
+});
